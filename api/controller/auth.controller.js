b88ce92847da24f880e3dd0360e310a8567b1b57
Omprasadbl <omprasadbl@gmail.com>
2024-06-22 16:44:33 +0530

    signup api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,4 +1,6 @@
 import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs'
+
 export const signup= async(req,res)=>{
     const {username,password,email}= req.body;
 
@@ -6,8 +8,15 @@ export const signup= async(req,res)=>{
     {
         return res.status(400).json({message:"Please fill all the fields."});
     }
+
+    const hashedPassword=bcryptjs.hashSync(password,10);
+
     // if objects key value name same you can use name one's
-    const newUser= new User({username,email,password});
-     await newUser.save();
+    const newUser= new User({username,email,password:hashedPassword});
+     try {
+        await newUser.save();
      res.status(201).json(newUser);
+     } catch (error) {
+        res.status(500).json({message:error.message})
+     }
 }
