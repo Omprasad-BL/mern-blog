af575e25bdd8d2d34919e732a4b60d9cca8647b1
Omprasadbl <omprasadbl@gmail.com>
2025-10-16 22:40:30 +0530

    User Profile added

--- client/src/components/FooterComponent.jsx
+++ client/src/components/FooterComponent.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import {BsFacebook, BsTwitter,BsInstagram, BsGithub,BsDribbble} from 'react-icons/bs'
 export default function FooterComponent() {
   return (
-    <Footer container className="border border-t-8 border-teal-500">
+    <Footer container className="border border-t-2 border-teal-500">
       <div className="w-full max-w-7xl mx-auto">
         <div className="grid w-full justify-between  sm:flex  md:grid-cols-1">
           <div className="mt-5">
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,64 +1,88 @@
-import { Navbar,TextInput } from 'flowbite-react'
+import { Avatar, Dropdown, DropdownItem, Navbar, TextInput } from "flowbite-react";
 import { Button } from "flowbite-react";
 
-import { Link,useLocation } from 'react-router-dom'
-import {AiOutlineSearch} from 'react-icons/ai'
-import {FaMoon} from 'react-icons/fa'
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+import { useSelector } from "react-redux";
+
 export default function Header() {
-    const path=useLocation().pathname;
+  const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
-    <Navbar className='border-b-2 flex justify-between'>
-    <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-        <span className='px-2 py-1  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Adiomi</span>
+    <Navbar className="border-b-2 flex justify-between">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-1  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+          Adiomi
+        </span>
         Blog
-    </Link> 
-    <form>
-        <TextInput type='text' className='hidden lg:inline'
-            placeholder='Search...'
-            rightIcon={AiOutlineSearch}
-
+      </Link>
+      <form>
+        <TextInput
+          type="text"
+          className="hidden lg:inline"
+          placeholder="Search..."
+          rightIcon={AiOutlineSearch}
         />
-    </form>
-    <Button className='w-12 h-10 lg:hidden' color='gray' pill>
-        <AiOutlineSearch/>
-    </Button>
-    <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h-10 hidden sm:inline' color="gray" pill>
-            <FaMoon/>
+      </form>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+          <FaMoon />
         </Button>
-        <Link to='/sign-in'>
-            <Button gradientDuoTone='purpleToBlue' outline pill>
-                Sign In
+        {currentUser ? (
+          <Dropdown arrowIcon={false} inline
+          label={
+            <Avatar
+                alt="user"
+                img={currentUser.profilePicture}
+                rounded
+            />
+          } >
+
+          <Dropdown.Header>
+            <span className="block text-sm p-2">@{currentUser.username}</span>
+            <span className="block text-sm font-medium truncate p-2">{currentUser.email}</span>
+          </Dropdown.Header>
+          <Link to="/dashboard?tab=profile">
+            <Dropdown.Item>
+            Profile
+            </Dropdown.Item>
+          </Link>
+          <Dropdown.Divider/>
+            <Dropdown.Item>
+                SignOut
+            </Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to="/sign-in">
+            <Button gradientDuoTone="purpleToBlue" outline pill>
+              Sign In
             </Button>
-        </Link>
-        <Navbar.Toggle/>
-    </div>
+          </Link>
+        )}
+        <Navbar.Toggle />
+      </div>
 
-    <Navbar.Collapse>
-        <Navbar.Link active={path==='/'}>
-            <Link to='/'>
-                Home
-            </Link>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
 
-            </Navbar.Link>    
+        <Navbar.Link active={path === "/about"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
 
-            <Navbar.Link active={path==='/about'}>
-            <Link to='/about'>
-                About
-            </Link>
-
-            </Navbar.Link>  
-
-            <Navbar.Link active={path==='/projects'}>
-            <Link to='/projects'>
-                Projects
-            </Link>
-
-            </Navbar.Link>   
-
-        </Navbar.Collapse>
-     
-
+        <Navbar.Link active={path === "/projects"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
-  )
+  );
 }
