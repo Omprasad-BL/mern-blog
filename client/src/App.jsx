29f543b362d2c60941d3e229a39a1d0c592ee910
Omprasadbl <omprasadbl@gmail.com>
2025-12-03 22:08:58 +0530

    make the dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Projects from './pages/Projects'
 import About from './pages/About'
 import Header from './components/Header'
 import FooterComponent from './components/FooterComponent'
+import PrivateRoute from './components/PrivateRoute'
 export default function App() {
   return (
     <BrowserRouter>
@@ -18,7 +19,9 @@ export default function App() {
         <Route path='/about' element={<About/>}/>
         <Route path='/sign-in' element={<SignIn/>}/>
         <Route path='/sign-up' element={<SignUp/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route element={<PrivateRoute/>}>
+            <Route path='/dashboard' element={<Dashboard/>}/>
+        </Route>
         <Route path='/projects' element={<Projects/>}/>
       </Routes>
       <FooterComponent/>
